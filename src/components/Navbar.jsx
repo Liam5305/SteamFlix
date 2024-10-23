de3f21854972169943c92b1a571410c1f1bb8d0b
Liam Rutherford <liamrutherford10@gmail.com>
2024-10-23 13:59:52 +0100

    Added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
+import GameHiveLogo from '../assets/GameHive.png'
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 w-full bg-gray-800 bg-opacity-90 shadow-lg z-50">
+    <nav className="fixed top-0 w-full bg-gray-800 bg-opacity-90 shadow-lg z-50 py-4">
       <div className="container mx-auto px-6">
         <div className="flex items-center h-16">
           {/* Left side - Logo and Navigation */}
-          <div className="flex items-center space-x-8">
+          <div className="flex items-center space-x-4">
+            <img 
+              src={GameHiveLogo} 
+              alt="GameHive Logo" 
+              className="h-[100px] w-[100px] object-contain" // Tailwind classes for size and object fit
+            />
             <Link to="/" className="text-2xl font-bold text-yellow-400 hover:text-yellow-300 transition-colors">
               GameHive
             </Link>
